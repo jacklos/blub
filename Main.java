912a873181e8248e29bef4a9cf4b6c5640d20473
jacklos <michael.lubowiecki@gmail.com>
2023-06-23 10:25:25 +0200

    Add files via upload
    Main-Class

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame("Mein erstes GUI-Projekt!");
+		
+		frame.setSize(400, 600); // Größe des Fensters bestimmen
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Beendet das Programm, sobald das Fenster geschlossen wird.
+		frame.setVisible(true); // Sichtbarkeit
+		
+
+	}
+
+}
